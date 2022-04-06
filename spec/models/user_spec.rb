41943dea55110755969733ed469ee9bc7c6b8c5b
Jtakk <Junoid1@gmail.com>
2022-04-07 00:26:11 +0900

    Create User model and set validations

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  let(:user) { build(:user) }
+  let(:name_less_user) { build(:user, name: "") }
+  let(:email_less_user) { build(:user, email: "") }
+  let(:long_name_user) { build(:user, name: "a" * 31) }
+  let(:long_email_user) { build(:user, email: "a" * 244 + "@example.com") }
+  let(:valid_addresses_user_list) { build_list(:user, 5) }
+  let(:invalid_addresses_user_list) { build_list(:user, 6) }
+
+  it "has a valid value" do
+    expect(user).to be_valid
+  end
+
+  describe "name validation" do
+    it "is invalid without a name" do
+      expect(name_less_user).not_to be_valid
+      name_less_user.valid?
+      expect(name_less_user.errors.messages[:name]).to include("can't be blank")
+    end
+
+    it "is invalid with a too long name" do
+      expect(long_name_user).not_to be_valid
+      long_name_user.valid?
+      expect(long_name_user.errors.messages[:name]).
+        to include("is too long (maximum is 30 characters)")
+    end
+  end
+
+  describe "email validation" do
+    let(:valid_addresses) do
+      %w(foo@bar.com FOO@bar.COM F_O-O@bar.baz.org foo.bar@baz.jp foo+bar@baz.cn)
+    end
+    let(:invalid_addresses) do
+      %w(foo@bar,com foo_bar.org foo.bar@baz foo@bar_baz.com foo@bar+baz.com foo@bar..com)
+    end
+
+    before do
+      valid_addresses_user_list.map.with_index { |user, i| user.email = valid_addresses[i] }
+      invalid_addresses_user_list.map.with_index { |user, i| user.email = invalid_addresses[i] }
+    end
+
+    it "is invalid without a email" do
+      expect(email_less_user).not_to be_valid
+      email_less_user.valid?
+      expect(email_less_user.errors.messages[:email]).to include("can't be blank")
+    end
+
+    it "is invalid with a too long email" do
+      expect(long_email_user).not_to be_valid
+      long_email_user.valid?
+      expect(long_email_user.errors.messages[:email]).
+        to include("is too long (maximum is 255 characters)")
+    end
+
+    it "is valid with a email formatted correctly" do
+      valid_addresses_user_list.each do |user|
+        expect(user).to be_valid
+      end
+    end
+
+    it "is invalid with a email formatted incorrectly" do
+      invalid_addresses_user_list.each do |user|
+        expect(user).not_to be_valid
+      end
+    end
+
+    it "is invalid with a duplicated email" do
+      duplicated_user = user.dup
+      duplicated_user.email.upcase!
+      user.save
+      expect(duplicated_user).not_to be_valid
+    end
+
+    it "converts email to lower-case before save" do
+      mixed_case_email = "TesT@eXamPle.CoM"
+      user.email = mixed_case_email
+      user.save
+      expect(user.email).to eq mixed_case_email.downcase
+    end
+  end
+end
