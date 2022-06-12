9687548eeb2c73b46529d7b5a2732f030bb14c61
Jtakk <Junoid1@gmail.com>
2022-06-12 15:53:22 +0900

    Add an initializer file for webpacker

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,3 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require('webpack')
-
-environment.plugins.prepend('env',
-  new webpack.EnvironmentPlugin(['REACT_APP_GMA_KEY'])
-)
 
 module.exports = environment
