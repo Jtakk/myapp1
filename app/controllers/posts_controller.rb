fb33fb94b25119d7216b99a5008c524cd3a05e04
Jtakk <Junoid1@gmail.com>
2022-08-16 13:52:35 +0900

    Apply pagination to a list of the user's posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   MAX_IMAGE_COUNT = 10
+  MAX_ITEM_COUNT = 10
   before_action :ajax_logged_in_user, only: [:create]
   before_action :logged_in_user, only: [:update, :destroy]
   before_action :correct_user, only: [:update, :destroy]
@@ -7,6 +8,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:id])
     @posts = @user.posts.latest.as_json(include: { mountain: { only: [:name, :yomi] } })
+    @max_item_count = MAX_ITEM_COUNT
   end
 
   def show
--- app/javascript/components/UserPosts.jsx
+++ app/javascript/components/UserPosts.jsx
@@ -6,18 +6,40 @@ import Stack from '@mui/material/Stack';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import LikeIndicator from './LikeIndicator';
+import Pagination from '@mui/material/Pagination';
 
 const CustomPaper = styled(Paper)({
   padding: '8px',
   textDecoration: 'none',
 });
 
-const UserPosts = (props) => {
+const UserPosts = ({posts, currentUser, maxItemCount}) => {
+  const [page, setPage] = React.useState(1);
+  const totalItemCount = posts.length;
+  const itemCount = maxItemCount;
+  const pageCount = Math.ceil(totalItemCount / itemCount);
+  const [displayedItems, setDisplayedItems] = React.useState([]);
+  const handleChange = (event, value) => {
+    setPage(value);
+  };
+  React.useEffect(() => {
+    setDisplayedItems(posts.slice((page - 1) * itemCount, page * itemCount));
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }, [page]);
+
   return (
     <Box sx={{ minHeight: '100%', bgcolor: '#f5f5f5' }}>
       <Container maxWidth="md" sx={{ py: 5 }}>
+        {totalItemCount == 0
+          ? <Typography variant="body1" align="right" sx={{ mb: 2 }}>0件</Typography>
+          : <Typography variant="body1" align="right" sx={{ mb: 2 }}>{((page-1)*itemCount+1)+"〜"+((page-1)*itemCount+displayedItems.length)+"件を表示 / "+totalItemCount+"件中"}</Typography>
+        }
         <Stack spacing={2}>
-          {props.posts.map((post, i) => (
+          {displayedItems.map((post, i) => (
             <CustomPaper component="a" href={"/posts/"+post.id} elevation={3} key={i}>
               <Typography variant="body2">{post.mountain.yomi}</Typography>
               <Typography variant="h4">{post.mountain.name}</Typography>
@@ -26,10 +48,13 @@ const UserPosts = (props) => {
               <Typography variant="body2">{post.created_at}</Typography>
               <Typography variant="body2">{post.updated_at}</Typography>
               <Typography variant="body1">{post.message}</Typography>
-              <LikeIndicator post={post} currentUser={props.currentUser} />
+              <LikeIndicator post={post} currentUser={currentUser} />
             </CustomPaper>
           ))}
         </Stack>
+        <Box sx={{ display: 'flex', justifyContent: 'center', my: 5 }}>
+          <Pagination count={pageCount} page={page} onChange={handleChange} />
+        </Box>
       </Container>
     </Box>
   );
